931037ad14e726377f4fc56f84bfbbecf76c0926
Peter Ebueku <pebueku@gmail.com>
2023-02-04 01:11:03 +0100

    Updated Dockerfile, docker-compose member.entity, app.module, main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { Member } from './members/entities/member.entity';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
